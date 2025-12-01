a2c597ce7d4d0cd7ff8ffcdac210a62399493413
Clément LATHIERE <clement.lathiere22@outlook.fr>
2025-12-01 16:47:16 +0100

    ALL CHANGING

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Welcome to CineFinder 🎬</h1>;
+  return <h1 style={{ color: "white" }}>Welcome to CineFinder 🎬</h1>;
 };
+
 
 export default Home;
